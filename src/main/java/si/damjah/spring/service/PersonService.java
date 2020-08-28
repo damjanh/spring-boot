b9e7f2b60e55b6be3608e39d58b47025da993641
damjanh <damjan.hudobreznik@gmail.com>
2020-08-28 14:27:29 +0200

    Implement person controller and service.

---
+++ src/main/java/si/damjah/spring/service/PersonService.java
@@ -0,0 +1,22 @@
+package si.damjah.spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import si.damjah.spring.dao.PersonDao;
+import si.damjah.spring.model.Person;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("Fake") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+}
